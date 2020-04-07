21cffc75b52df8882d489bfd6777077b96a16d27
Ravi Maurya <ravi.rm.maurya@gmail.com>
2020-04-07 22:40:49 +0530

    Base_Code

--- base.cpp
+++ base.cpp
@@ -29,3 +29,13 @@ int main(){
     cin.tie(NULL); cout.tie(NULL);
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #pragma GCC optimize ("-O3")
+
+// int main(){
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(NULL); cout.tie(NULL);
+//     return 0;
+// }
