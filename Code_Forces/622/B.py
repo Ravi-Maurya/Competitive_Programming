07d8e377d5c0e7a1f3c6b1107edf7af64e3877ca
Ravi Maurya <ravi.rm.maurya@gmail.com>
2020-04-19 21:02:44 +0530

    CF delete

--- Code_Forces/622/B.py
+++
@@ -1,8 +0,0 @@
-def solve():
-	n,x,y = map(int,input().split())
-	
-
-if __name__ == '__main__':
-	T = int(input())
-	for _ in range(T):
-		solve()
--- Code_Forces/624/B.py
+++
@@ -1,7 +0,0 @@
-def solve():
-	pass
-
-if __name__ == '__main__':
-	t = int(input())
-	for _ in range(t):
-		solve();
