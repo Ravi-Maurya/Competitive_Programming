50584d24f9942fe32be186ad3bca77f0f1dc7265
Ravi Maurya <ravi.rm.maurya@gmail.com>
2020-04-20 07:21:23 +0530

    Base File

--- base.cpp
+++ base.cpp
@@ -1,41 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 #pragma GCC optimize ("-O3")
-using ll = long long;
-using ld = long double;
-const ll mod = 1000000007;
-const ll inf = 1000000000000000000;
-const ll rk = 256;
-
-#define eb emplace_back
-#define pb push_back
-#define mp make_pair
-#define vc vector
-#define fs first
-#define sec second
-#define pq priority_queue
-#define lb lower_bound
-#define ub upper_bound
-#define pll pair<ll,ll> 
-#define pls pair<ll,string>
-#define psl pair<string,ll>
-#define plc pair<ll,char>
-#define pcl pair<char,ll>
-#define pss pair<string,string>
-#define T ll t=0;cin>>t;for(ll test=0;test<t;test++)
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
     return 0;
 }
-
-// #include <bits/stdc++.h>
-// using namespace std;
-// #pragma GCC optimize ("-O3")
-
-// int main(){
-//     ios_base::sync_with_stdio(0);
-//     cin.tie(NULL); cout.tie(NULL);
-//     return 0;
-// }
