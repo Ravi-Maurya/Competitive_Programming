ac4607771ed07be1a37bed28c6643d04dab708b5
Ravi Maurya <ravi.rm.maurya@gmail.com>
2020-05-03 12:43:01 +0530

    Stacks_and_Queues

--- Interview_Bit/Stacks_and_Queues/Largest_rectangle_in_histogram.cpp
+++ Interview_Bit/Stacks_and_Queues/Largest_rectangle_in_histogram.cpp
@@ -2,17 +2,18 @@ int Solution::largestRectangleArea(vector<int> &A) {
     int n = A.size(),i;
     stack<int> st;
     int res = 0,top,area;
-    while(i<n){
+    for(i=0; i<n; i++){
         if(st.empty() || A[st.top()]<=A[i]){
             st.push(i);
-            i++;
+            continue;
         }
-        else{
+        while(!st.empty() && A[i]<A[st.top()]){
             top = st.top();
             st.pop();
             area = A[top]*(st.empty() ? i: i-1-st.top());
             res = max(res,area);
         }
+        st.push(i);
     }
     while(!st.empty()){
         top = st.top();
