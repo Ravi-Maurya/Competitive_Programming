0a737f27b22bedf996ef847bc487ab3b005fdc46
Ravi Maurya <ravi.rm.maurya@gmail.com>
2020-06-20 23:12:39 +0530

    CodeChef

---
+++ Code_Chef/Practice/Flow016.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize ("-O3")
+
+#define ll long long int
+#define pll pair<ll,ll> 
+#define pls pair<ll,string>
+#define psl pair<string,ll>
+#define plc pair<ll,char>
+#define pcl pair<char,ll>
+#define pss pair<string,string>
+#define maxheap(type) priority_queue<type>
+#define minheap(type) priority_queue<type,vector<type>,greater<type>>
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define precise(x,y) fixed<<setprecision(y)<<x
+#define tol(s) transform(s.begin(),s.end(),s.begin(),::tolower);
+#define tou(s) transform(s.begin(),s.end(),s.begin(),::toupper);
+#define rep(n) for(auto i=0; i<n; i++)
+
+int gcd(int a, int b){
+    while(b){
+        a %= b;
+        swap(a,b);
+    }
+    return a;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL); cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,g,l;
+        cin>>a>>b;
+        g = gcd(a,b);
+        cout<< g << " " << (a/g)*b <<"\n";
+    }
+    return 0;
+}
