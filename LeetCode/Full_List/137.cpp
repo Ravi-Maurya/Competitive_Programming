c356a480df5bd0848a0bb84549382666fdb828da
Ravi Maurya <ravi.rm.maurya@gmail.com>
2020-06-23 21:03:47 +0530

    LeetCode Full List

---
+++ LeetCode/Full_List/137.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size()==0)
+            return 0;
+        if(nums.size()==1)
+            return nums[0];
+        sort(nums.begin(),nums.end());
+        for(int i=0 ;i<nums.size()-2;i++){
+            if(nums[i+2] == nums[i])
+                i += 2;
+            else
+                return nums[i];
+        }
+        return nums[nums.size()-1];
+    }
+};
