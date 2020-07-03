577e80cdc387868fce54f6c4443c927f58a9d330
Ravi Maurya <ravi.rm.maurya@gmail.com>
2020-07-03 09:01:54 +0530

    InterviewBit

--- Interview_Bit/Dynamic_Programming/Evaluate_Expression_to_true.cpp
+++ Interview_Bit/Dynamic_Programming/Evaluate_Expression_to_true.cpp
@@ -1,66 +1,29 @@
-struct Box{
-	int T,F;
-	Box():T(0),F(0){}
-};
-Box util(char c,Box b1, Box b2){
-	Box b;
-	if(c=='|'){
-		b.T=b1.T*b2.F+b1.F*b2.T+b1.T*b2.T;
-		b.F=b1.F*b2.F;
-	}
-	else if(c=='&'){
-		b.F=b1.T*b2.F+b1.F*b2.T+b1.F*b2.F;
-		b.T=b1.T*b2.T;
-	}
-	else {
-		b.F=b1.F*b2.F+b1.T*b2.T;
-		b.T=b1.F*b2.T+b1.T*b2.F;
-	}
-	b.T%=1003;
-	b.F%=1003;
-	return b;
-}
-Box method(Box **arr,int i,int l,string s1, string s2){
-	int m=0,n=s1.length();
-	Box b1;
-	for(m=i;m<l;m++){
-		Box b2;
-		b2=util(s2[m],arr[i][n-m-1],arr[m+1][n-l-1]);
-		b1.T+=b2.T;
-		b1.F+=b2.F;
-		b1.T%=1003;
-		b1.F%=1003;
-
-	}
-	return b1;
-}
+const int mod = 1e3 + 3;
 int Solution::cnttrue(string A) {
-    string s=A;
-    string s1="",s2="";
-	int i,j,k,l;
-	for(i=0;i<s.length();i++){
-		if(s[i]=='T' | s[i]=='F')
-			s1+=s[i];
-		else s2+=s[i];
-	}
-	int n=s1.length();
-	Box **arr=new Box *[n];
-	for(i=0;i<n;i++)
-		arr[i]=new Box[n-i];
-	
-	for(i=0;i<n;i++){
-		Box b1;
-		if(s1[i]=='T')
-			b1.T=1;
-		else b1.F=1;
-		arr[i][n-i-1]=b1;
-	}
-	for(k=1;k<n;k++){
-		i=0;j=n-k-1;l=k;
-		while(i<n-k){
-			arr[i][j]=method(arr,i,l,s1,s2);
-			i++;j--;l++;
-		}
-	}
-	return arr[0][0].T;
+    int n = A.size();
+    vector<vector<int>> True(n+1,vector<int>(n+1,0)), False(n+1,vector<int>(n+1,0));
+    for(int i=0; i<n; i+=2){
+        True[i][i] = (A[i]=='T');
+        False[i][i] = (A[i]=='F');
+    }
+    for(int length = 3; length<=n; length+=2){
+        for(int i=0; i<n-length+1; i++){
+            int j = i+length-1;
+            for(int k=i+1; k<j;k+=2){
+                if(A[k]=='|'){
+                    True[i][j] = (True[i][j]%mod + (True[i][k-1]*True[k+1][j])%mod + (True[i][k-1]*False[k+1][j])%mod + (False[i][k-1]*True[k+1][j])%mod)%mod;
+                    False[i][j] = (False[i][j]%mod + (False[i][k-1]*False[k+1][j])%mod)%mod;
+                }
+                else if(A[k]=='&'){
+                    True[i][j] = (True[i][j]%mod + (True[i][k-1]*True[k+1][j])%mod)%mod;
+                    False[i][j] = (False[i][j]%mod + (False[i][k-1]*False[k+1][j])%mod  + (True[i][k-1]*False[k+1][j])%mod + (False[i][k-1]*True[k+1][j])%mod)%mod;
+                }
+                else if(A[k]=='^'){
+                    True[i][j] = (True[i][j]%mod + (True[i][k-1]*False[k+1][j])%mod + (False[i][k-1]*True[k+1][j])%mod)%mod;
+                    False[i][j] = (False[i][j]%mod + (False[i][k-1]*False[k+1][j])%mod + (True[i][k-1]*True[k+1][j])%mod )%mod;
+                }
+            }
+        }
+    }
+    return True[0][n-1];
 }
