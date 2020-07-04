b6e7d2545386153d2d669b01ac4d482afa0bbd5a
Ravi Maurya <ravi.rm.maurya@gmail.com>
2020-07-04 10:43:52 +0530

    IB

--- IB.cpp
+++ IB.cpp
@@ -1,9 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-const long long mod = 1000000007;
-const long long inf = 1000000000000000000;
-const long long rk = 256;
-class Solution{
-    public:
-    
-};
+
+int main(){
+    return 0;
+}
