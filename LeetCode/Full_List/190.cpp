e54f1202c142ef339b5b7812c95af08590c0316e
Ravi Maurya <ravi.rm.maurya@gmail.com>
2020-07-12 16:27:34 +0530

    LeetCode Full List

---
+++ LeetCode/Full_List/190.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        for(int i=0; i<32; i++){
+            int bit = (1<<i) & n;
+            if(bit!=0){
+                res += (1<<(31-i));
+            }
+        }
+        return res;
+    }
+};
