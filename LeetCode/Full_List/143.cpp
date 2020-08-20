2f29731f34676664a9ef94640ed5934f49e90895
Ravi Maurya <ravi.rm.maurya@gmail.com>
2020-08-20 14:22:46 +0530

    LeetCode Full List

--- LeetCode/Full_List/143.cpp
+++ LeetCode/Full_List/143.cpp
@@ -45,3 +45,47 @@ public:
         }
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(!head || !head->next)
+            return;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* curr = slow->next;
+        slow->next = NULL;
+        ListNode* prev = NULL;
+        while(curr){
+            ListNode* tmp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        curr = head;
+        while(curr && prev){
+            ListNode* tmp = curr->next;
+            curr->next = prev;
+            curr = prev->next;
+            prev->next = tmp;
+            prev = curr;
+            curr = tmp;
+        }
+        return;
+    }
+};
